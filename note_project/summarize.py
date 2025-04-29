bb2b7dbf3e2055c5a9fb08dec6b3c0aa25f7e3f1
Lion2410 <afolayanchampion@gmail.com>
2025-04-29 19:17:28 +0100

    initial commit

---
+++ note_project/summarize.py
@@ -0,0 +1,12 @@
+# summarize.py
+
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+
+def summarize_text(text, sentence_count=5):
+    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    summarizer = LsaSummarizer()
+    summary = summarizer(parser.document, sentence_count)
+    summarized_text = ' '.join(str(sentence) for sentence in summary)
+    return summarized_text
